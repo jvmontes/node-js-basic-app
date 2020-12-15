3e3dc6d022b850118af367b2161cdb68a440b51a
Jorge Viramontes <>
2020-12-14 21:37:19 -0600

    Remove unnecessary conversion from array to dictionary, use dictionary initially instead.

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ var languageStrings = [
     }
 ]
 
-var copyArray = [
-    {
-        "key": "who-am-i-1",
-        "en-copy": "My name is Jorge Viramontes. I am a first-generation college student. I am also a first-generation American citizen, born of Mexican immigrant parents.",
-        "sp-copy": "Mi nombre es Jorge Viramontes. Soy la primera generacion de mi familia de atender a la universidad. Tambien soy la primera generacion americana de mi familia, nacido de padres imigrantes de México."
-    }, {
-        "key": "who-am-i-2",
-        "en-copy": "test2",
-        "sp-copy": "prueba2"
+var copyDictionary = {
+    who_am_i_1: {
+        en_copy: "My name is Jorge Viramontes. I am a first-generation college student. I am also a first-generation American citizen, born of Mexican immigrant parents.",
+        sp_copy: "Mi nombre es Jorge Viramontes. Soy la primera generacion de mi familia de atender a la universidad. Tambien soy la primera generacion americana de mi familia, nacido de padres imigrantes de México."
+    },
+    who_am_i_2: {
+        en_copy: "test2",
+        sp_copy: "prueba2"
     }
-]
+}
 
 var currentLanguage;
 
@@ -23,27 +22,15 @@ var languageTranslator = function({
     dropdownId = "jvLanguageSelector",
     stringAttribute = "data-jv-text",
     chosenLanguage = "English",
-    languages = languageStrings,
 } = {}) {
     const root = document.documentElement;
 
-    var copyDictionary = {};
-
-    copyArray.forEach(copyItem => {
-        copyDictionary[copyItem.key] = {
-            enCopy: copyItem["en-copy"],
-            spCopy: copyItem["sp-copy"]
-        }
-    })
-
     console.log("copy dictionary - ", copyDictionary);
 
-    var listOfLanguages = Object.keys(languages[0]);
     currentLanguage = chosenLanguage;
 
     (function createLanguageDropdown() {
         var languageDropdown = document.getElementById(dropdownId);
-
         languageDropdown.addEventListener("change", function(e) {
             currentLanguage = languageDropdown[languageDropdown.selectedIndex].value;
             resolveAllLanguageStrings();
@@ -58,7 +45,6 @@ var languageTranslator = function({
             stringToBeResolved.textContent = resolvedText;
         });
     }
-
 };
 
 function resolveString(originalText, languageStrings) {

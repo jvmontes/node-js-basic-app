3280a8704654a1b0e5514d38e1aa1728dac4f531
Jorge Viramontes <>
2020-12-24 16:00:54 -0600

    Added ejs dependency and nodemon dev dependency; add routing to root, home, and about in server.js; Use header partial to include header across two pages. Use EJS to pass name from server to page. Reference: https://www.freecodecamp.org/news/develop-deploy-first-fullstack-web-app/#templatingbasics

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
-// Load Node modules
+// Load modules
 var express = require('express');
+const ejs = require('ejs');
 
 // Initialize Express
 var app = express();
+
 // Render static files
 app.use(express.static('public'));
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // Port website will run on
 app.listen(8080);
+
+// *** GET Routes - display pages ***
+
+// Root Route
+app.get('/', function (req, res) {
+    var name = 'Jorge from ejs';
+    res.render('pages/index', {
+        name : name
+    });
+});
+
+// Home Route - same as Root
+app.get('/home', function (req, res) {
+    var name = 'Jorge from ejs';
+    res.render('pages/index', {
+        name : name
+    });
+});
+
+// About Route
+app.get('/about', function (req, res) {
+    res.render('pages/about');
+});
